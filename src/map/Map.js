dc87d0ebaed57c3cc1ce36aec7e5923b9c2f16c7
Tim Weiland <tim@weiland-lahnstein.de>
2022-10-18 15:58:10 +0200

    Fix bug that let points escape the image
    "display: inline-block" makes sure that the parent div is as large as
    the contained image, so that the relative coordinates of the points
    match the image exactly.

--- src/map/Map.js
+++ src/map/Map.js
@@ -3,7 +3,7 @@ import Point from '../points/Point.js';
 import MapImage from '../assets/map_sketch.jpg';
 
 export default function Map({coordinates}) {
-    return  <div style={{position:"relative"}}>
+    return  <div style={{position:"relative", display: "inline-block"}}>
                 <img src={MapImage} alt="Map" className="map" style={{opacity:0.6}} />
                 <div> <Point coordinates={coordinates}/> </div>
             </div>
