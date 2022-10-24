d6da5349d380281ae96ba937ab8ac38ea4eecbcd
Tim Weiland <tim@weiland-lahnstein.de>
2022-10-24 16:39:41 +0200

    Enable line drawing

--- src/map/Map.js
+++ src/map/Map.js
@@ -12,23 +12,94 @@ export default function Map({coordinates}) {
 } 
 */
 
-export default function Map({coordinates}) {
+export default function Map({ coordinates }) {
     const canvasRef = useRef();
+    let lines = []
+    let curLineStart = [0, 0];
+
+    const image = new Image();
+
+    function drawBackgroundImage(canvas) {
+        const ctx = canvas.getContext('2d');
+        canvas.width = image.width;
+        canvas.height = image.height;
+        ctx.drawImage(image, 0, 0, image.width, image.height, 0, 0, canvas.width, canvas.height);
+    }
 
     useEffect(() => {
         const canvas = canvasRef.current;
         const ctx = canvas.getContext("2d");
 
-        const image = new Image();
         image.src = require('../assets/map_sketch.jpg');
         image.onload = (res) => {
-            canvas.width = image.width;
-            canvas.height= image.height;
-            ctx.drawImage(image, 0, 0, image.width, image.height, 0, 0, canvas.width, canvas.height);
+            drawBackgroundImage(canvas);
         }
 
-        ctx.fillStyle="#000000";
-    }, [])
+        ctx.fillStyle = "#000000";
+    }, []);
 
-    return <canvas ref={canvasRef} style={{height: '100%', border: '1px solid black' }}/>
+    function getMousePos(evt) {
+        const canvas = canvasRef.current;
+        const rect = canvas.getBoundingClientRect();
+        const scaleX = canvas.width / rect.width;
+        const scaleY = canvas.height / rect.height;
+        return {
+            x: (evt.clientX - rect.left) * scaleX,
+            y: (evt.clientY - rect.top) * scaleY
+        };
+    }
+
+
+    function addLineStart(x, y) {
+        curLineStart = [x, y];
+    }
+
+    function clearCanvas(canvas) {
+        const context = canvas.getContext('2d');
+        // Store the current transformation matrix
+        context.save();
+
+        // Use the identity matrix while clearing the canvas
+        context.setTransform(1, 0, 0, 1, 0, 0);
+        context.clearRect(0, 0, canvas.width, canvas.height);
+
+        // Restore the transform
+        context.restore();
+    }
+
+    function redrawAll() {
+        const canvas = canvasRef.current;
+        const ctx = canvas.getContext("2d");
+        clearCanvas(canvas);
+        drawBackgroundImage(canvas);
+        ctx.lineWidth = 7;
+        lines.map((line) => {
+            ctx.beginPath();
+            ctx.moveTo(line[0][0], line[0][1]);
+            ctx.lineTo(line[1][0], line[1][1]);
+            ctx.stroke();
+            return null;
+        })
+    }
+
+    function addLineEnd(x, y) {
+        lines.push([curLineStart, [x, y]]);
+        redrawAll();
+    }
+
+    function startDrawing(evt) {
+        const mousePos = getMousePos(evt);
+        addLineStart(mousePos.x, mousePos.y);
+    }
+
+    function finishDrawing(evt) {
+        const mousePos = getMousePos(evt);
+        addLineEnd(mousePos.x, mousePos.y);
+    }
+
+    return <canvas ref={canvasRef} style={{ height: '100%', border: '1px solid black' }} 
+    onMouseDown={startDrawing}
+    onMouseUp={finishDrawing}
+
+    />
 }
