c20f6ce8ee37b88023425d6c767f38cbf9dddf6b
authenticdreamer <tim.weisbarth@student.uni-tuebingen.de>
2022-11-30 12:37:57 +0100

    .

---
+++ src/taskbar/taskbar.js
@@ -0,0 +1,56 @@
+
+
+
+
+
+<div class="taskbar-container" style={{position: "absolute", top: `${0}%`, left: `${50}%` }}>
+          <div class="taskbar">
+              <div class="button"><FontAwesomeIcon icon={faQuestion} /></div>
+          </div>
+
+          <div class="taskbar">
+            <div class="button"  onClick={undo}>
+              <FontAwesomeIcon icon={faRotateLeft} />
+            </div>
+          </div>
+
+          <div class="taskbar">
+            <div class="button">
+              <Link to="/byebye" style={{ textDecoration: 'none' }} onClick={() => {
+                toggleAITree(true);
+              }}>
+                <FontAwesomeIcon icon={faCheck} />
+              </Link>
+            </div>
+          </div>
+          <div class="taskbar">
+            <div class="button">
+              <Link to="/" style={{ textDecoration: 'none' }} onClick={cleanUp}>
+                <FontAwesomeIcon icon={faXmark} />
+              </Link>
+            </div>
+          </div>
+        </div>
+
+        <div class="taskbar-container" style={{top: `${0}%`, right: `${50}%` }}>
+
+          <div class="taskbar">
+            <div class="legend">10€: </div>
+            <div class="legend">
+              <div class="point" style={{width: "20px", height: "20px"}}/>
+            </div>
+          </div>
+
+          <div class="taskbar">
+            <div class="legend">20€: </div>
+            <div class="legend"> 
+              <div class="point" style={{width: "35px", height: "35px"}}/>
+            </div> 
+          </div>
+          <div clasS="taskbar">
+            <div class="legend"> 30€: </div>
+            <div class="legend">
+              <div class="point" style={{width: "50px", height: "50px"}}/>
+            </div>
+          </div>
+        </div>
