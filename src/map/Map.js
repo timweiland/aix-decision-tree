254ffbb3e64d544f31b9683936b8b80ad2a13b6b
dorothee-sigg <doro_sigg@web.de>
2022-12-02 17:21:00 +0100

    first steps towards drawing on the touchscreen

--- src/map/Map.js
+++ src/map/Map.js
@@ -1,243 +1,341 @@
-import './Map.css';
-import Point from '../points/Point.js';
-import MapImage from '../assets/map_sketch.jpg';
-import { useState, useEffect, useRef } from 'react';
-import './button.css';
+// add touch based on this tutorial
+// https://zipso.net/a-simple-touchscreen-sketchpad-using-javascript-and-html5/
+// still need to fix: prevent from returning to previous page
 
-export default function Map({ coordinates, tree, splitTree, highlightNode, unhighlightAll, enableInteraction }) {
-    const canvasRef = useRef();
-    const [isDrawing, setIsDrawing] = useState(false);
-    const [curLineStart, setCurLineStart] = useState([0, 0]);
-    const [curLineEnd, setCurLineEnd] = useState([0, 0]);
-    const [highlightedRect, setHighlightedRect] = useState(null);
+import "./Map.css";
+import Point from "../points/Point.js";
+import MapImage from "../assets/map_sketch.jpg";
+import { useState, useEffect, useRef } from "react";
+import "./button.css";
 
-    const image = new Image();
+export default function Map({
+  coordinates,
+  tree,
+  splitTree,
+  highlightNode,
+  unhighlightAll,
+  enableInteraction,
+}) {
+  const canvasRef = useRef();
+  const [isDrawing, setIsDrawing] = useState(false);
+  const [curLineStart, setCurLineStart] = useState([0, 0]);
+  const [curLineEnd, setCurLineEnd] = useState([0, 0]);
+  const [highlightedRect, setHighlightedRect] = useState(null);
 
-    function drawBackgroundImage(canvas) {
-        const ctx = canvas.getContext('2d');
-        canvas.width = image.width;
-        canvas.height = image.height;
-        ctx.drawImage(image, 0, 0, image.width, image.height, 0, 0, canvas.width, canvas.height);
-    }
+  const image = new Image();
 
-    useEffect(() => {
-        const canvas = canvasRef.current;
-        const ctx = canvas.getContext("2d");
+  function drawBackgroundImage(canvas) {
+    const ctx = canvas.getContext("2d");
+    canvas.width = image.width;
+    canvas.height = image.height;
+    ctx.drawImage(
+      image,
+      0,
+      0,
+      image.width,
+      image.height,
+      0,
+      0,
+      canvas.width,
+      canvas.height
+    );
+  }
 
-        image.src = require('../assets/map_prototype.jpg');
-        image.onload = (res) => {
-            redrawAll();
-        }
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
 
-        ctx.fillStyle = "#000000";
-    }, [image, isDrawing, curLineEnd, tree]);
+    image.src = require("../assets/map_prototype.jpg");
+    image.onload = (res) => {
+      redrawAll();
+    };
 
-    function relativeToAbsoluteCoords(canvas, x, y) {
-        const rect = canvas.getBoundingClientRect();
-        return [(x / 100.) * canvas.width, (y / 100.) * canvas.height]
-    }
+    ctx.fillStyle = "#000000";
+  }, [image, isDrawing, curLineEnd, tree]);
 
-    function absoluteToRelativeCoords(canvas, x, y) {
-        const rect = canvas.getBoundingClientRect();
-        return [100. * x / canvas.width, 100. * y / canvas.height]
-    }
+  function relativeToAbsoluteCoords(canvas, x, y) {
+    const rect = canvas.getBoundingClientRect();
+    return [(x / 100) * canvas.width, (y / 100) * canvas.height];
+  }
 
-    function getMousePos(evt) {
-        const canvas = canvasRef.current;
-        const rect = canvas.getBoundingClientRect();
-        const scaleX = canvas.width / rect.width;
-        const scaleY = canvas.height / rect.height;
+  function absoluteToRelativeCoords(canvas, x, y) {
+    const rect = canvas.getBoundingClientRect();
+    return [(100 * x) / canvas.width, (100 * y) / canvas.height];
+  }
+
+  function getMousePos(evt) {
+    const canvas = canvasRef.current;
+    const rect = canvas.getBoundingClientRect();
+    const scaleX = canvas.width / rect.width;
+    const scaleY = canvas.height / rect.height;
+    return {
+      x: (evt.clientX - rect.left) * scaleX,
+      y: (evt.clientY - rect.top) * scaleY,
+    };
+  }
+
+  function getTouchPos(evt) {
+    const canvas = canvasRef.current;
+    const rect = canvas.getBoundingClientRect();
+    const scaleX = canvas.width / rect.width;
+    const scaleY = canvas.height / rect.height;
+    if (evt.touches) {
+      if (evt.touches.length == 1) {
+        var touch = evt.touches[0];
         return {
-            x: (evt.clientX - rect.left) * scaleX,
-            y: (evt.clientY - rect.top) * scaleY
+          x: (touch.pageX - touch.target.offsetLeft - rect.left) * scaleX,
+          y: (touch.pageY - touch.target.offsetTop - rect.top) * scaleY,
         };
+      }
     }
+  }
 
+  function sketchpad_touchStart(evt) {
+    //getTouchPos();
+    startDrawing(evt);
 
-    function addLineStart(x, y) {
-        setCurLineStart([x, y]);
+    evt.preventDefault();
+  }
+
+  function sketchpad_touchMove(evt) {
+    //getTouchPos(evt);
+
+    whileDrawing(evt);
+
+    evt.preventDefault();
+  }
+
+  function sketchpad_touchEnd(evt) {
+    //getTouchPos(evt);
+
+    // During a touchmove event, unlike a mousemove event, we don't need to check if the touch is engaged, since there will always be contact with the screen by definition.
+    finishDrawing(evt);
+
+    evt.preventDefault();
+  }
+
+  function addLineStart(x, y) {
+    setCurLineStart([x, y]);
+  }
+
+  function treeNodeToCanvasRect(node, canvas) {
+    let [treeX0, treeY0, treeX1, treeY1] = node.rect;
+    [treeX0, treeY0] = relativeToAbsoluteCoords(canvas, treeX0, treeY0);
+    [treeX1, treeY1] = relativeToAbsoluteCoords(canvas, treeX1, treeY1);
+    return [treeX0, treeY0, treeX1 - treeX0, treeY1 - treeY0];
+  }
+
+  function highlightTreeNode(x, y) {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+    const [x_rel, y_rel] = absoluteToRelativeCoords(canvas, x, y);
+
+    const curTreeNode = tree.find(x_rel, y_rel);
+    highlightNode(curTreeNode);
+    setHighlightedRect(treeNodeToCanvasRect(curTreeNode, canvas));
+  }
+
+  function clearCanvas(canvas) {
+    const context = canvas.getContext("2d");
+    // Store the current transformation matrix
+    context.save();
+
+    // Use the identity matrix while clearing the canvas
+    context.setTransform(1, 0, 0, 1, 0, 0);
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    // Restore the transform
+    context.restore();
+  }
+
+  function drawCoordinates(coordinates, ctx, canvas) {
+    let minRent = 1000;
+    let maxRent = -1000;
+    coordinates.forEach((coord) => {
+      const rent = coord[2];
+      if (rent < minRent) {
+        minRent = rent;
+      }
+      if (rent > maxRent) {
+        maxRent = rent;
+      }
+    });
+    const [minSize, maxSize] = [5, 20];
+    coordinates.forEach((c) => {
+      const rent = c[2];
+      const size = Math.floor(
+        minSize + ((rent - minRent) * (maxSize - minSize)) / (maxRent - minRent)
+      );
+      ctx.fillRect(
+        (c[0] * canvas.width) / 100,
+        (c[1] * canvas.height) / 100,
+        size,
+        size
+      );
+    });
+  }
+
+  function redrawAll() {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+    // Clear everything that has been drawn so far
+    clearCanvas(canvas);
+    drawBackgroundImage(canvas);
+
+    ctx.lineWidth = 7;
+    ctx.fillStyle = "#000000";
+    // Draw all of the lines the user has drawn so far
+    tree.get_lines().map((line) => {
+      ctx.beginPath();
+      ctx.moveTo(...relativeToAbsoluteCoords(canvas, line[0][0], line[0][1]));
+      ctx.lineTo(...relativeToAbsoluteCoords(canvas, line[1][0], line[1][1]));
+      ctx.stroke();
+      return null;
+    });
+    // Draw the rent points on the map
+    drawCoordinates(coordinates, ctx, canvas);
+    if (isDrawing && curLineEnd) {
+      // Dashed line that the user is currently in the process of drawing
+      ctx.setLineDash([7, 7]);
+      ctx.beginPath();
+      ctx.moveTo(curLineStart[0], curLineStart[1]);
+      let [x1, y1] = curLineEnd;
+      if (highlightedRect) {
+        x1 = Math.min(
+          highlightedRect[0] + highlightedRect[2],
+          Math.max(highlightedRect[0], x1)
+        );
+        y1 = Math.min(
+          highlightedRect[1] + highlightedRect[3],
+          Math.max(highlightedRect[1], y1)
+        );
+      }
+      ctx.lineTo(x1, y1);
+      ctx.stroke();
+      ctx.setLineDash([]);
     }
-
-    function treeNodeToCanvasRect(node, canvas) {
-        let [treeX0, treeY0, treeX1, treeY1] = node.rect;
-        [treeX0, treeY0] = relativeToAbsoluteCoords(canvas, treeX0, treeY0);
-        [treeX1, treeY1] = relativeToAbsoluteCoords(canvas, treeX1, treeY1);
-        return [treeX0, treeY0, treeX1 - treeX0, treeY1 - treeY0]
+    // Paint each tree leaf in a different color
+    const treeLeaves = tree.get_leaves();
+    treeLeaves.forEach((leaf) => {
+      ctx.globalAlpha = 0.6;
+      ctx.fillStyle = leaf.color.hex();
+      ctx.fillRect(...treeNodeToCanvasRect(leaf, canvas));
+      ctx.globalAlpha = 1.0;
+    });
+    if (isDrawing && highlightedRect) {
+      // Highlight the area of the tree that the user is currently drawing inside
+      ctx.globalAlpha = 0.6;
+      ctx.fillStyle = "#CBC3E3";
+      ctx.fillRect(...highlightedRect);
+      ctx.globalAlpha = 1.0;
     }
-
-    function highlightTreeNode(x, y) {
-        const canvas = canvasRef.current;
-        const ctx = canvas.getContext("2d");
-        const [x_rel, y_rel] = absoluteToRelativeCoords(canvas, x, y);
-
-        const curTreeNode = tree.find(x_rel, y_rel);
-        highlightNode(curTreeNode);
-        setHighlightedRect(treeNodeToCanvasRect(curTreeNode, canvas));
+    const testPointNode = tree.getTestPointNode();
+    if (testPointNode !== undefined) {
+      ctx.globalAlpha = 0.8;
+      ctx.fillStyle = "#FF0000";
+      ctx.fillRect(...treeNodeToCanvasRect(testPointNode, canvas));
+      ctx.globalAlpha = 1.0;
     }
+  }
 
-    function clearCanvas(canvas) {
-        const context = canvas.getContext('2d');
-        // Store the current transformation matrix
-        context.save();
-
-        // Use the identity matrix while clearing the canvas
-        context.setTransform(1, 0, 0, 1, 0, 0);
-        context.clearRect(0, 0, canvas.width, canvas.height);
-
-        // Restore the transform
-        context.restore();
+  function addLineEnd(x, y) {
+    const canvas = canvasRef.current;
+    const dx = curLineStart[0] - x;
+    const dy = curLineStart[1] - y;
+    const [x0, y0] = absoluteToRelativeCoords(
+      canvas,
+      curLineStart[0],
+      curLineStart[1]
+    );
+    const curTreeNode = tree.find(x0, y0);
+    const [treeX0, treeY0, treeX1, treeY1] = curTreeNode.rect;
+    let line = null;
+    let splitAxis = null;
+    let splitPos = null;
+    if ((2 * dx ** 2) / (dx ** 2 + dy ** 2) <= 1) {
+      // Horizontal line
+      line = [
+        [x0, treeY0],
+        [x0, treeY1],
+      ];
+      splitAxis = 0;
+      splitPos = x0;
+    } else {
+      // Vertical line
+      line = [
+        [treeX0, y0],
+        [treeX1, y0],
+      ];
+      splitAxis = 1;
+      splitPos = y0;
     }
+    splitTree(curTreeNode.idx, splitAxis, splitPos, line);
+  }
 
-    function drawCoordinates(coordinates, ctx, canvas) {
-        let minRent = 1000;
-        let maxRent = -1000;
-        coordinates.forEach((coord) => {
-            const rent = coord[2];
-            if(rent < minRent) {
-                minRent = rent;
-            }
-            if(rent > maxRent) {
-                maxRent = rent;
-            }
-        });
-        const [minSize, maxSize] = [5, 20];
-        coordinates.forEach((c) => {
-            const rent = c[2];
-            const size = Math.floor(minSize + (rent - minRent) * (maxSize - minSize) / (maxRent - minRent));
-            ctx.fillRect(c[0] * canvas.width / 100, c[1] * canvas.height / 100, size, size);
-        })
+  function startDrawing(evt) {
+    if (!enableInteraction) {
+      return;
     }
-
-    function redrawAll() {
-        const canvas = canvasRef.current;
-        const ctx = canvas.getContext("2d");
-        // Clear everything that has been drawn so far
-        clearCanvas(canvas);
-        drawBackgroundImage(canvas);
-
-        ctx.lineWidth = 7;
-        ctx.fillStyle = "#000000";
-        // Draw all of the lines the user has drawn so far
-        tree.get_lines().map((line) => {
-            ctx.beginPath();
-            ctx.moveTo(...relativeToAbsoluteCoords(canvas, line[0][0], line[0][1]));
-            ctx.lineTo(...relativeToAbsoluteCoords(canvas, line[1][0], line[1][1]));
-            ctx.stroke();
-            return null;
-        })
-        // Draw the rent points on the map
-        drawCoordinates(coordinates, ctx, canvas);
-        if(isDrawing && curLineEnd) {
-            // Dashed line that the user is currently in the process of drawing
-            ctx.setLineDash([7, 7]);
-            ctx.beginPath();
-            ctx.moveTo(curLineStart[0], curLineStart[1]);
-            let [x1, y1] = curLineEnd;
-            if(highlightedRect) {
-                x1 = Math.min(highlightedRect[0] + highlightedRect[2], Math.max(highlightedRect[0], x1));
-                y1 = Math.min(highlightedRect[1] + highlightedRect[3], Math.max(highlightedRect[1], y1));
-            }
-            ctx.lineTo(x1, y1);
-            ctx.stroke();
-            ctx.setLineDash([]);
-        }
-        // Paint each tree leaf in a different color
-        const treeLeaves = tree.get_leaves();
-        treeLeaves.forEach((leaf) => {
-            ctx.globalAlpha = 0.6;
-            ctx.fillStyle = leaf.color.hex();
-            ctx.fillRect(...treeNodeToCanvasRect(leaf, canvas));
-            ctx.globalAlpha = 1.0;
-        })
-        if(isDrawing && highlightedRect) {
-            // Highlight the area of the tree that the user is currently drawing inside
-            ctx.globalAlpha = 0.6;
-            ctx.fillStyle = "#CBC3E3";
-            ctx.fillRect(...highlightedRect);
-            ctx.globalAlpha = 1.0;
-        }
-        const testPointNode = tree.getTestPointNode();
-        if(testPointNode !== undefined) {
-            ctx.globalAlpha = 0.8;
-            ctx.fillStyle = "#FF0000";
-            ctx.fillRect(...treeNodeToCanvasRect(testPointNode, canvas));
-            ctx.globalAlpha = 1.0;
-        }
-    }
-
-    function addLineEnd(x, y) {
-        const canvas = canvasRef.current;
-        const dx = curLineStart[0] - x;
-        const dy = curLineStart[1] - y;
-        const [x0, y0] = absoluteToRelativeCoords(canvas, curLineStart[0], curLineStart[1]);
-        const curTreeNode = tree.find(x0, y0);
-        const [treeX0, treeY0, treeX1, treeY1] = curTreeNode.rect;
-        let line = null;
-        let splitAxis = null;
-        let splitPos = null;
-        if (2 * dx ** 2 / (dx ** 2 + dy ** 2) <= 1) {
-            // Horizontal line
-            line = [[x0, treeY0], [x0, treeY1]]
-            splitAxis = 0;
-            splitPos = x0;
-        }
-        else {
-            // Vertical line
-            line = [[treeX0, y0], [treeX1, y0]];
-            splitAxis = 1;
-            splitPos = y0;
-        }
-        splitTree(curTreeNode.idx, splitAxis, splitPos, line);
-    }
-
-    function startDrawing(evt) {
-        if(!enableInteraction) {
-            return;
-        }
-        const mousePos = getMousePos(evt);
+    /* const mousePos = getMousePos(evt);
         highlightTreeNode(mousePos.x, mousePos.y);
         addLineStart(mousePos.x, mousePos.y);
         setCurLineEnd([mousePos.x, mousePos.y]);
-        setIsDrawing(true);
-    }
+        setIsDrawing(true); */
+    const touchPos = getTouchPos(evt);
+    highlightTreeNode(touchPos.x, touchPos.y);
+    addLineStart(touchPos.x, touchPos.y);
+    setCurLineEnd([touchPos.x, touchPos.y]);
+    setIsDrawing(true);
+  }
 
-    function finishDrawing(evt) {
-        if(!enableInteraction) {
-            return;
-        }
-        if (isDrawing) {
-            const mousePos = getMousePos(evt);
-            addLineEnd(mousePos.x, mousePos.y);
-        }
-        setCurLineStart([0, 0]);
-        setCurLineEnd([0, 0]);
-        setIsDrawing(false);
-        unhighlightAll();
+  function finishDrawing(evt) {
+    if (!enableInteraction) {
+      return;
     }
+    if (isDrawing) {
+      // const mousePos = getMousePos(evt);
+      const touchPos = getTouchPos(evt);
+      addLineEnd(touchPos.x, touchPos.y);
+    }
+    setCurLineStart([0, 0]);
+    setCurLineEnd([0, 0]);
+    setIsDrawing(false);
+    unhighlightAll();
+  }
 
-    function whileDrawing(evt) {
-        if(!enableInteraction) {
-            return;
-        }
-        if(isDrawing) {
-            const mousePos = getMousePos(evt);
-            setCurLineEnd([mousePos.x, mousePos.y]);
-        }
+  function whileDrawing(evt) {
+    if (!enableInteraction) {
+      return;
     }
-    function leaveCanvas(evt) {
-        if(!enableInteraction) {
-            return;
-        }
-        setIsDrawing(false);
-        unhighlightAll();
-        setCurLineStart([0, 0]);
-        setCurLineEnd([0, 0]);
+    if (isDrawing) {
+      // const mousePos = getMousePos(evt);
+      const touchPos = getTouchPos(evt);
+      setCurLineEnd([touchPos.x, touchPos.y]);
     }
+  }
+  function leaveCanvas(evt) {
+    if (!enableInteraction) {
+      return;
+    }
+    setIsDrawing(false);
+    unhighlightAll();
+    setCurLineStart([0, 0]);
+    setCurLineEnd([0, 0]);
+  }
 
-    return <canvas ref={canvasRef} style={{ height: '100%', border: '1px solid black' }}
-        onMouseDown={startDrawing}
+  return (
+    <canvas
+      ref={canvasRef}
+      style={{ height: "100%", border: "1px solid black" }}
+      /*onMouseDown={startDrawing}
         onMouseUp={finishDrawing}
         onMouseMove={whileDrawing}
-        onMouseLeave={leaveCanvas}
+        onMouseLeave={leaveCanvas}*/
+
+      onTouchStart={sketchpad_touchStart}
+      onTouchEnd={sketchpad_touchEnd}
+      onTouchMove={sketchpad_touchMove}
+      onTouchCancel={leaveCanvas}
     />
+  );
 }
