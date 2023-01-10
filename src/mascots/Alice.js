c7240fbe29ff7cd53db8b17f3b09155994a8a60b
authenticdreamer <tim.weisbarth@student.uni-tuebingen.de>
2023-01-10 09:45:39 +0100

    .

--- src/mascots/Alice.js
+++ src/mascots/Alice.js
@@ -3,10 +3,10 @@ import aliceImg from "../assets/alice.png";
 
 export default function Alice({ message, excited }) {
     return (
-        <div className="absolute bottom-28 right-20 flex flex-row">
+        <div className="absolute bottom-28 right-10 flex flex-row">
             { message &&
                 <div className="chat chat-end" style={{placeItems: "start"}}>
-                    <div className="chat-bubble ml-10 br-0 mr-0 chat-bubble-secondary text-2xl">
+                    <div className="chat-bubble chat-bubble-secondary text-2xl ml-3 mr-0">
                         {message}
                     </div>
                 </div>
