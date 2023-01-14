9b8c3392a5504263d8c7640590c66569a2e7de65
dorothee-sigg <doro_sigg@web.de>
2023-01-15 00:56:24 +0100

    taskbar changes: €/qm + white border on points

--- src/taskbar/taskbar.js
+++ src/taskbar/taskbar.js
@@ -1,9 +1,6 @@
 import "./taskbar.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faQuestion,
-  faRotateLeft,
-} from "@fortawesome/free-solid-svg-icons";
+import { faQuestion, faRotateLeft } from "@fortawesome/free-solid-svg-icons";
 
 export default function Taskbar({ undo, openTutorial }) {
   return (
@@ -16,7 +13,15 @@ export default function Taskbar({ undo, openTutorial }) {
         left: `${0}%`,
       }}
     >
-      <div class="taskbar-container" style={{ position: "absolute", top: `${0}%`, left: `${80}%`, width: "20%" }}>
+      <div
+        class="taskbar-container"
+        style={{
+          position: "absolute",
+          top: `${0}%`,
+          left: `${80}%`,
+          width: "20%",
+        }}
+      >
         <div class="taskbar">
           <div class="button" onClick={openTutorial}>
             <FontAwesomeIcon icon={faQuestion} />
@@ -30,36 +35,45 @@ export default function Taskbar({ undo, openTutorial }) {
         </div>
       </div>
 
-      <div class="taskbar-container" style={{ position: "absolute", top: `${0}%`, left: `${70}%`, width: "10%" }}></div>
+      <div
+        class="taskbar-container"
+        style={{
+          position: "absolute",
+          top: `${0}%`,
+          left: `${70}%`,
+          width: "10%",
+        }}
+      ></div>
 
-
-      <div class="taskbar-container" style={{ top: `${0}%`, right: `${30}%`, width: "70%" }}>
-        <div className="text-white">
-          Preise pro Quadratmeter:
-        </div>
+      <div
+        class="taskbar-container"
+        style={{ top: `${0}%`, right: `${30}%`, width: "70%" }}
+      >
+        <div className="text-white">€ / qm:</div>
         <div className="w-1"></div>
         <div class="taskbar">
-          <div class="legend">10€: </div>
+          <div class="legend">10: </div>
           <div class="legend">
             <div class="point" style={{ width: "15px", height: "15px" }} />
           </div>
         </div>
 
         <div class="taskbar">
-          <div id="taskbar20" class="legend">20€: </div>
+          <div id="taskbar20" class="legend">
+            20:{" "}
+          </div>
           <div class="legend">
             <div class="point" style={{ width: "27px", height: "27px" }} />
           </div>
         </div>
 
         <div clasS="taskbar">
-          <div class="legend"> 30€: </div>
+          <div class="legend"> 30: </div>
           <div class="legend">
             <div class="point" style={{ width: "40px", height: "40px" }} />
           </div>
         </div>
-
       </div>
     </div>
-  )
+  );
 }
