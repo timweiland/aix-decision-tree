fe5a5f959b43f2ca270894491c5bde77c65b2ebc
authenticdreamer <tim.weisbarth@student.uni-tuebingen.de>
2023-01-16 13:20:37 +0100

    Additional screen before showAITree screen

--- src/screens/InitialScreen.js
+++ src/screens/InitialScreen.js
@@ -9,15 +9,23 @@ import Map from '../map/Map';
 import Tree from '../tree/Tree';
 import Popup from '../popup/Popup';
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faArrowRightLong
+} from "@fortawesome/free-solid-svg-icons";
+
 export default function InitialScreen({ cleanUp, userTree, mietdaten, undo, splitTree, highlightNode, unhighlightAll, onComplete, openTutorial }) {
     const [isDone, setIsDone] = useState(false);
+    const [showPopUp, setShowPopUp] = useState(false);
     console.log(userTree);
 
     const NoOfUserLines = userTree.structure.get_lines().length;
 
     useEffect(() => {
         if (NoOfUserLines === 5) {
+            setShowPopUp(true);
             setIsDone(true);
+            
         }
     }, [NoOfUserLines])
 
@@ -39,11 +47,24 @@ export default function InitialScreen({ cleanUp, userTree, mietdaten, undo, spli
             </TreeColumn>
 
             {
-                (isDone) &&
-                <Popup closeCallback={onComplete} icon="check">
+                (isDone) && (showPopUp) &&
+                <Popup closeCallback={() => {setShowPopUp(false)}} icon="check">
                     <p>Super! Du hast deinen Entscheidungsbaum fertig gestellt.</p>
-                    <p>Jetzt wird auf der rechten Seite erscheinen, wie die KI die Stadt in Bereiche mit unterschiedlich hohen Mieten unterteilt.</p>
+                    <p>Gleich wird auf der rechten Seite erscheinen, wie die KI die Stadt in Bereiche mit unterschiedlich hohen Mieten unterteilt.</p>
                 </Popup>
+            }
+
+            {
+                (isDone) && 
+                <div
+                  className="absolute hover:cursor-pointer bg-green-700 rounded-3xl bottom-8 right-8 pl-16 pr-16 shadow-2xl shadow-green-700 opacity-80 text-white"
+                  style={{ fontSize: "50pt" }}
+                  onClick={() => {
+                    onComplete();
+                  }}>
+                    <FontAwesomeIcon icon={faArrowRightLong} />
+                    
+                </div>
             }
         </ColumnContainer>
 
