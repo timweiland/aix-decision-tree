1fbfc4619d5a285f3be339c06dfc3c827b7685eb
Tim Weiland <tim@weiland-lahnstein.de>
2023-01-16 15:37:19 +0100

    Remove icons in global comparison

--- src/screens/quantitativeComparison/AccuracyComparison.js
+++ src/screens/quantitativeComparison/AccuracyComparison.js
@@ -8,12 +8,12 @@ export default function AccuracyComparison({avgDiffUser, avgDiffAI}) {
     return (
         <div className="stats shadow-md bg-gray-50">
             <div className="stat">
-                <div className="stat-figure text-primary text-4xl"><FontAwesomeIcon icon={faPerson} /></div>
+                {/* <div className="stat-figure text-primary text-4xl"><FontAwesomeIcon icon={faPerson} /></div> */}
                 <div className="stat-title">Deine Genauigkeit</div>
                 <div className={classNames("stat-value", { "text-green-700": isUserBetter, "text-orange-700": isAIBetter })}>{"+/- " + avgDiffUser + "€"}</div>
             </div>
             <div className="stat">
-                <div className="stat-figure text-primary text-4xl"><FontAwesomeIcon icon={faRobot} /></div>
+                {/* <div className="stat-figure text-primary text-4xl"><FontAwesomeIcon icon={faRobot} /></div> */}
                 <div className="stat-title">KI-Genauigkeit</div>
                 <div className={classNames("stat-value", { "text-green-700": isAIBetter, "text-orange-700": isUserBetter })}>{"+/- " + avgDiffAI + "€"}</div>
             </div>
