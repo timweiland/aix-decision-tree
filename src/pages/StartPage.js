a318811784e58bf2f3d8f80f17c0e0690fbd1295
dorothee-sigg <doro_sigg@web.de>
2023-01-18 12:46:35 +0100

    centered speechbubbles + 'Klicke um bob zu helfen'

--- src/pages/StartPage.js
+++ src/pages/StartPage.js
@@ -14,16 +14,16 @@ function StartPage() {
       </div>
 
       <div className="middle-column">
-        <div className="chat chat-end">
-          <div className="chat-bubble chat-bubble-info text-6xl top-8 my-10 shadow-2xl">
+        <div className="chat chat-start">
+          <div className="chat-bubble chat-bubble-error text-6xl top-8 my-10 shadow-2xl text-center">
             <div className="m-7">
               Wir sind neu in Tübingen und wollen wissen, wie hoch die Miete für
               ein WG-Zimmer ist!
             </div>
           </div>
         </div>
-        <div className="chat chat-start">
-          <div className="chat-bubble chat-bubble-error text-6xl my-10 shadow-2xl">
+        <div className="chat chat-end">
+          <div className="chat-bubble chat-bubble-info text-6xl my-10 shadow-2xl text-center">
             <div className="m-7">Hey, hast Du Lust uns zu helfen?</div>
           </div>
         </div>
