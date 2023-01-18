1097c28c7e8e4749daf6aff32086c4a5c034d23a
authenticdreamer <tim.weisbarth@student.uni-tuebingen.de>
2023-01-18 11:17:35 +0100

    cleaner Zur Lösung der KI

--- src/screens/InitialScreen.js
+++ src/screens/InitialScreen.js
@@ -96,6 +96,7 @@ export default function InitialScreen({
 
             {
                 (isDone) && 
+                <div>
                 <div
                   className="absolute hover:cursor-pointer bg-green-700 rounded-3xl bottom-14 right-8 pl-16 pr-16 shadow-2xl shadow-green-700 opacity-80 text-white"
                   style={{ fontSize: "50pt" }}
@@ -107,13 +108,12 @@ export default function InitialScreen({
                     
                      
                 </div>
-            }
-            {
-                (isDone) && 
                 <div
                 className="absolute  bottom-5 right-10 text-black text-2xl"
                 >Zur Lösung der KI</div>
+                </div>
             }
+            
         </ColumnContainer>
 
     );
