b5917ded8527dafd3a808362191872b9d7f2896d
Tim Weiland <tim@weiland-lahnstein.de>
2023-01-18 11:34:40 +0100

    Implement chat bubble pings

--- src/mascots/Alice.js
+++ src/mascots/Alice.js
@@ -1,12 +1,29 @@
 import classNames from "classnames";
+import { useEffect, useState } from "react";
 import aliceImg from "../assets/alice.png";
 
 export default function Alice({ message, excited }) {
+  const [hasNewMessage, setHasNewMessage] = useState(false);
+
+  useEffect(() => {
+    setHasNewMessage(true);
+    const timerID = setTimeout(() => setHasNewMessage(false), 5000);
+    return () => {
+      clearTimeout(timerID);
+    }
+  }, [message]);
+
   return (
     <div className="absolute bottom-28 right-10 flex flex-row z-30">
       {message && (
         <div className="chat chat-end" style={{ placeItems: "start" }}>
-          <div className="chat-bubble chat-bubble-info text-2xl ml-3 mr-0 shadow-2xl">
+          <div className="chat-bubble chat-bubble-info text-2xl ml-3 mr-0 shadow-2xl relative">
+            {hasNewMessage &&
+              <span class="flex absolute h-6 w-6 top-0 right-0 -mt-1 -mr-1">
+                <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-400 opacity-75"></span>
+                <span class="relative inline-flex rounded-full h-6 w-6 bg-red-500"></span>
+              </span>
+            }
             {message}
           </div>
         </div>
--- src/mascots/Bob.js
+++ src/mascots/Bob.js
@@ -1,7 +1,18 @@
 import classNames from "classnames";
+import { useEffect, useState } from "react";
 import bobImg from "../assets/bob.png";
 
 export default function Bob({ message, excited }) {
+  const [hasNewMessage, setHasNewMessage] = useState(false);
+
+  useEffect(() => {
+    setHasNewMessage(true);
+    const timerID = setTimeout(() => setHasNewMessage(false), 5000);
+    return () => {
+      clearTimeout(timerID);
+    }
+  }, [message]);
+
   return (
     <div className="absolute bottom-10 left-10 flex flex-row z-30">
       <img
@@ -12,6 +23,12 @@ export default function Bob({ message, excited }) {
       {message && (
         <div className="chat chat-start">
           <div className="chat-bubble chat-bubble-error text-2xl shadow-2xl">
+            {hasNewMessage &&
+              <span class="flex absolute h-6 w-6 top-0 right-0 -mt-1 -mr-1">
+                <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-400 opacity-75"></span>
+                <span class="relative inline-flex rounded-full h-6 w-6 bg-red-500"></span>
+              </span>
+            }
             {message}
           </div>
         </div>
