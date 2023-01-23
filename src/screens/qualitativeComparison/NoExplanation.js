72a6f3b7a0dd7253255f947adbce0f5956988c98
dorothee-sigg <doro_sigg@web.de>
2023-01-23 16:45:32 +0100

    arrow button nach 'schritt für schritt'

--- src/screens/qualitativeComparison/NoExplanation.js
+++ src/screens/qualitativeComparison/NoExplanation.js
@@ -1,18 +1,18 @@
-import { useState, useEffect } from 'react';
-import Lottie from 'lottie-react';
+import { useState, useEffect } from "react";
+import Lottie from "lottie-react";
 
-import PredictionComparison from './PredictionComparison';
-import ColumnContainer from '../../columns/ColumnContainer';
-import MapColumn from '../../columns/MapColumn';
-import TreeColumn from '../../columns/TreeColumn';
+import PredictionComparison from "./PredictionComparison";
+import ColumnContainer from "../../columns/ColumnContainer";
+import MapColumn from "../../columns/MapColumn";
+import TreeColumn from "../../columns/TreeColumn";
 
-import Map from '../../map/Map';
-import Tree from '../../tree/Tree';
+import Map from "../../map/Map";
+import Tree from "../../tree/Tree";
 
-import Alice from '../../mascots/Alice';
-import Bob from '../../mascots/Bob';
+import Alice from "../../mascots/Alice";
+import Bob from "../../mascots/Bob";
 
-import confettiAnimation from '../../assets/confetti_anim.json';
+import confettiAnimation from "../../assets/confetti_anim.json";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faForward, faArrowRightLong } from "@fortawesome/free-solid-svg-icons";
 
@@ -20,8 +20,15 @@ function displayedRent(rent) {
   return Number(rent).toFixed(1).replace(".", ",");
 }
 
-export default function NoExplanation({ mietdaten, userTree, setUserTree, aiTree, setAITree, testPoint, onComplete }) {
-
+export default function NoExplanation({
+  mietdaten,
+  userTree,
+  setUserTree,
+  aiTree,
+  setAITree,
+  testPoint,
+  onComplete,
+}) {
   const x = testPoint[0];
   const y = testPoint[1];
   const [screenState, setScreenState] = useState("intro");
@@ -55,31 +62,33 @@ export default function NoExplanation({ mietdaten, userTree, setUserTree, aiTree
     setAIRentEstimate(aiNode.avgRent);
     setAITree({ structure: aiTree.structure, toggle: !aiTree.toggle });
     setShowAIRentEstimate(true);
-  }
+  };
 
   const cleanTestpoints = () => {
     userTree.structure.removeTestPoints();
     setUserTree(userTree.structure);
     aiTree.structure.removeTestPoints();
     setAITree({ structure: aiTree.structure, toggle: !aiTree.toggle });
-  }
+  };
 
   useEffect(() => {
     if (screenState === "intro") {
-      setBobMessage("Jetzt wollen wir den Mietpreis für diese Wohnung (gelber Punkt) wissen.")
-      setContinueHandler({ handler: () => {
-        setScreenState("followUserPath") 
-        setSkipHandler(undefined);
-      }
+      setBobMessage(
+        "Jetzt wollen wir den Mietpreis für diese Wohnung (gelber Punkt) wissen."
+      );
+      setContinueHandler({
+        handler: () => {
+          setScreenState("followUserPath");
+          setSkipHandler(undefined);
+        },
       });
       setSkipHandler({
         handler: () => {
           computeEstimates();
           setScreenState("final");
-        }
-      })
-    }
-    else if (screenState === "followUserPath") {
+        },
+      });
+    } else if (screenState === "followUserPath") {
       setBobMessage(undefined);
       setSkipHandler(undefined);
       userPath.forEach((node, node_idx) => {
@@ -91,15 +100,20 @@ export default function NoExplanation({ mietdaten, userTree, setUserTree, aiTree
             setTimeout(() => {
               setUserRentEstimate(node.avgRent);
               setShowUserRentEstimate(true);
-              setBobMessage(`Unser Baum schätzt eine Miete von ${displayedRent(node.avgRent)}€.`);
+              setBobMessage(
+                `Unser Baum schätzt eine Miete von ${displayedRent(
+                  node.avgRent
+                )}€.`
+              );
               setAliceMessage("Mal sehen, was mein KI-Baum schätzt.");
-              setContinueHandler({ handler: () => setScreenState("followAlicePath") });
+              setContinueHandler({
+                handler: () => setScreenState("followAlicePath"),
+              });
             }, 1000);
           }
         }, delay * node_idx);
       });
-    }
-    else if (screenState === "followAlicePath") {
+    } else if (screenState === "followAlicePath") {
       setBobMessage(undefined);
       setAliceMessage(undefined);
       setSkipHandler(undefined);
@@ -113,19 +127,22 @@ export default function NoExplanation({ mietdaten, userTree, setUserTree, aiTree
               setAIRentEstimate(node.avgRent);
               setShowAIRentEstimate(true);
               setBobMessage("Und welche Schätzung ist jetzt genauer?");
-              setAliceMessage(`Mein KI-Baum schätzt eine Miete von ${displayedRent(node.avgRent)}€.`);
+              setAliceMessage(
+                `Mein KI-Baum schätzt eine Miete von ${displayedRent(
+                  node.avgRent
+                )}€.`
+              );
               setContinueHandler({
                 handler: () => {
                   cleanTestpoints();
                   setScreenState("final");
-                }
+                },
               });
             }, 1000);
           }
         }, delay * node_idx);
       });
-    }
-    else if (screenState === "final") {
+    } else if (screenState === "final") {
       const trueRent = testPoint[2];
       setShowTrueRent(true);
       const userError = (userRentEstimate - trueRent) ** 2;
@@ -135,13 +152,15 @@ export default function NoExplanation({ mietdaten, userTree, setUserTree, aiTree
         setBobMessage("Super! Diese Miete haben wir genauer vorhergesagt!");
         setBobExcited(true);
         setAliceMessage("Oh, da hast du meine KI wohl geschlagen!");
-      }
-      else if (userError > aiError) {
-        setBobMessage("Da sind wir ein bisschen ungenauer. Aber das muss nichts heißen!");
-        setAliceMessage("Bei dieser Miete liegt meine KI ein bisschen näher dran.");
+      } else if (userError > aiError) {
+        setBobMessage(
+          "Da sind wir ein bisschen ungenauer. Aber das muss nichts heißen!"
+        );
+        setAliceMessage(
+          "Bei dieser Miete liegt meine KI ein bisschen näher dran."
+        );
         setAliceExcited(true);
-      }
-      else {
+      } else {
         setBobMessage("Erstaunlich!");
         setAliceMessage("Hier treffen beide Bäume die gleiche Schätzung!");
       }
@@ -150,45 +169,78 @@ export default function NoExplanation({ mietdaten, userTree, setUserTree, aiTree
         handler: () => {
           cleanTestpoints();
           onComplete();
-        }
+        },
       });
     }
-  }, [screenState])
+  }, [screenState]);
 
   return (
     <ColumnContainer>
-      {confetti && <Lottie className="absolute h-screen w-screen z-40" animationData={confettiAnimation} loop={false} />}
+      {confetti && (
+        <Lottie
+          className="absolute h-screen w-screen z-40"
+          animationData={confettiAnimation}
+          loop={false}
+        />
+      )}
       <MapColumn>
-        <Map coordinates={mietdaten} tree={userTree.structure} enableInteraction={false} testPoint={testPoint} hide={screenState === "followAlicePath"} />
-        {
-          bobMessage &&
-          <Bob message={bobMessage} excited={bobExcited} />
-        }
+        <Map
+          coordinates={mietdaten}
+          tree={userTree.structure}
+          enableInteraction={false}
+          testPoint={testPoint}
+          hide={screenState === "followAlicePath"}
+        />
+        {bobMessage && <Bob message={bobMessage} excited={bobExcited} />}
       </MapColumn>
 
       <TreeColumn>
         <div className="mt-4">
-          <Tree structure={userTree.structure} colors={userTree.structure.get_colors()} hide={screenState === "followAlicePath"} arrow="left" />
+          <Tree
+            structure={userTree.structure}
+            colors={userTree.structure.get_colors()}
+            hide={screenState === "followAlicePath"}
+            arrow="left"
+          />
         </div>
-        <PredictionComparison showUserRentEstimate={showUserRentEstimate} userRentEstimate={userRentEstimate} testPoint={testPoint}
-          showTrueRent={showTrueRent} showAIRentEstimate={showAIRentEstimate} aiRentEstimate={aiRentEstimate}
-          highlightAll={screenState === "final"} highlightUser={screenState === "followUserPath" && showUserRentEstimate}
-          highlightAI={screenState === "followAlicePath" && showAIRentEstimate} />
+        <PredictionComparison
+          showUserRentEstimate={showUserRentEstimate}
+          userRentEstimate={userRentEstimate}
+          testPoint={testPoint}
+          showTrueRent={showTrueRent}
+          showAIRentEstimate={showAIRentEstimate}
+          aiRentEstimate={aiRentEstimate}
+          highlightAll={screenState === "final"}
+          highlightUser={
+            screenState === "followUserPath" && showUserRentEstimate
+          }
+          highlightAI={screenState === "followAlicePath" && showAIRentEstimate}
+        />
         <div className="mt-2">
-          <Tree structure={aiTree.structure} colors={aiTree.structure.get_colors()} arrow="right" hide={screenState === "followUserPath" || screenState === "intro"} />
+          <Tree
+            structure={aiTree.structure}
+            colors={aiTree.structure.get_colors()}
+            arrow="right"
+            hide={screenState === "followUserPath" || screenState === "intro"}
+          />
         </div>
       </TreeColumn>
 
       <MapColumn>
-        <Map coordinates={mietdaten} tree={aiTree.structure} enableInteraction={false} testPoint={testPoint} hide={screenState === "followUserPath" || screenState === "intro"} />
-        {
-          aliceMessage &&
+        <Map
+          coordinates={mietdaten}
+          tree={aiTree.structure}
+          enableInteraction={false}
+          testPoint={testPoint}
+          hide={screenState === "followUserPath" || screenState === "intro"}
+        />
+        {aliceMessage && (
           <Alice message={aliceMessage} excited={aliceExcited} />
-        }
+        )}
       </MapColumn>
 
       <div className="flex absolute bottom-10 right-10 flex-col">
-        {skipHandler !== undefined &&
+        {skipHandler !== undefined && (
           <div
             className="hover:cursor-pointer bg-yellow-500 rounded-2xl pl-8 pr-8 shadow-2xl shadow-green-700 opacity-90 text-white font-medium h-25 z-50 border-transparent mb-4 text-center"
             style={{ fontSize: "60px" }}
@@ -199,7 +251,7 @@ export default function NoExplanation({ mietdaten, userTree, setUserTree, aiTree
           >
             Direkt zur Auflösung
           </div>
-        }
+        )}
         {continueHandler !== undefined && screenState === "intro" && (
           <div
             className="hover:cursor-pointer bg-green-700 rounded-2xl pl-8 pr-8 shadow-2xl shadow-green-700 opacity-90 text-white font-medium h-25 z-50 border-transparent text-center"
@@ -213,17 +265,17 @@ export default function NoExplanation({ mietdaten, userTree, setUserTree, aiTree
           </div>
         )}
         {continueHandler !== undefined && screenState !== "intro" && (
-        <div
-          className="hover:cursor-pointer bg-green-700 rounded-2xl bottom-10 right-10 pl-8 pr-8 shadow-2xl shadow-green-700 opacity-90 text-white h-25 z-50 border-transparent"
-          style={{ fontSize: "60px" }}
-          onClick={() => {
-            continueHandler.handler();
-            setContinueHandler(undefined);
-          }}
-        >
-          <FontAwesomeIcon icon={faArrowRightLong} />
-        </div>
-      )}
+          <div
+            className="absolute hover:cursor-pointer bg-green-700 rounded-2xl bottom-10 right-10 pl-8 pr-8 shadow-2xl shadow-green-700 opacity-90 text-white btn btn-lg h-25 z-50 border-transparent"
+            style={{ fontSize: "60px" }}
+            onClick={() => {
+              continueHandler.handler();
+              setContinueHandler(undefined);
+            }}
+          >
+            <FontAwesomeIcon icon={faArrowRightLong} />
+          </div>
+        )}
       </div>
     </ColumnContainer>
   );
