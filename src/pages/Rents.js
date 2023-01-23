705101dcf063095e7608dfd545871987834b967c
Tim Weiland <hello@timwei.land>
2023-01-23 16:16:13 +0100

    Images to explanation

--- src/pages/Rents.js
+++ src/pages/Rents.js
@@ -11,6 +11,7 @@ import algo1 from "../assets/algo_sketch1.png";
 import algo2 from "../assets/algo_sketch2.png";
 import algo3 from "../assets/algo_sketch3.png";
 import algo4 from "../assets/algo_sketch4.png";
+import algo5 from "../assets/algo_sketch5.png";
 
 function Rents() {
   return (
@@ -30,30 +31,39 @@ function Rents() {
             <div
             class="text-3xl text-black font-normal leading-relaxed"
             >
-               Für jede Linie berechnet sie die durchschnittliche Miete auf beiden Seiten der Linie. Schließlich wählt sie die Linie, wo der Unterschied zwischen echten Mietpreisen und durchschnittlicher Miete am kleinsten ist. [Checken die Leute das? Erwähnen, dass Ähnlichkeit der Mietpreise guter Indikator?]
+               Für jede Linie berechnet sie die durchschnittliche Miete auf beiden Seiten der Linie. Schließlich wählt sie die Linie, wo der Unterschied zwischen echten Mietpreisen und durchschnittlicher Miete am kleinsten ist. Das ist der Fall, wenn Bereiche mit ähnlichen Mietpreisen möglichst gut voneinander abgegrenzt werden.
             </div>
             <div
             class="text-3xl text-black font-normal leading-relaxed"
             >
-               Diese Berechnung wiederholt sie genauso für alle weiteren Unterteilungen. Am Ende verwendet sie die durchschnittlichen Mieten pro Bereich als Schätzung.
+               Diese Berechnung wiederholt sie genauso für alle weiteren Teilungen in Unterbereiche. Wenn sie fertig ist, verwendet sie die durchschnittlichen Mieten pro Bereich als Schätzung.
             </div>
             </Stack>
 
             <div
-            class="grid grid-cols-4"
-            style={{ fontSize: "50px",  marginLeft: "10.5vw", marginRight: "12.5vw", marginBottom: "7vh", marginTop: "25vh"}}
+            class="grid grid-cols-5 auto-cols-auto"
+            style={{ fontSize: "50px",  marginLeft: "11.5vw", marginRight: "12.5vw", marginBottom: "7vh", marginTop: "14.5vh"}}
             >
                 <div>
-                    <img style={{ height: "20vh"}} src={algo1} alt="algo1"/>
+                    <img style={{ height: "30vh"}} src={algo1} alt="algo1"/>
+                    <div class="text-base" style={{ marginLeft: "3.1vw"}}>Erste Linie ausprobieren</div>
                 </div>
                 <div>
-                    <img style={{ height: "20vh"}} src={algo2} alt="algo2"/>
+                    <img style={{ height: "30vh"}} src={algo2} alt="algo2"/>
+                    <div class="text-base" style={{ marginLeft: "3.1vw"}}>Durchschnittsmieten berechnen</div>
                 </div>
                 <div>
-                    <img style={{ height: "20vh"}} src={algo3} alt="algo3"/>
+                    <img style={{ height: "30vh"}} src={algo3} alt="algo3"/>                    
+                    <div class="text-base" style={{ marginLeft: "3.1vw"}}>Systematisch durchprobieren</div>
                 </div>
                 <div>
-                    <img style={{ height: "20vh"}} src={algo4} alt="algo4"/>
+                    <img style={{ height: "20vh"}} src={algo5} alt="algo5"/>                    
+                    {/*<div class="text-base" style={{ marginLeft: "3.1vw"}}>Systematisch durchprobieren</div>*/}
+                </div>                
+                <div>
+                    <img style={{ height: "30vh"}} src={algo4} alt="algo4"/>                    
+                    <div class="text-base" style={{ marginLeft: "3.1vw"}}>Beste Linie wählen</div>
+                    
                 </div>
 
             </div>
